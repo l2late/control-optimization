8a31d86a571ce8ba23a33faf2a45941469736183
nathantim <timmerhout@gmail.com>
2018-10-01 16:14:07 +0200

    Changes

--- assignment1/assignment1.m
+++ assignment1/assignment1.m
@@ -8,13 +8,13 @@ E2 = 8;
 E3 = 9;
 
 bc = (5 + E1) * 10e6; %battery cells
-bw = 4*10e3; %battery cells for W
-br = 6*10e3; %battery cells for R
+bw = 6*10e3; %battery cells for W
+br = 4*10e3; %battery cells for R
 hrw = 15; %hours required for W
 hrr = 10; %hours required for R
 nemp = 100 + E2; %number of employees
 hpm = 160; %hours per month per employee
-thpm = nemp * hpm;
+thpm = nemp * hpm; % total hours
 ms = 3000 + 50*E3; %monthly salary
 avs = (15 + E3) * 10e3; %available storage space
 rssw = 12; % required storage space W
@@ -39,7 +39,7 @@ lb = [0 0]; %lower bound
 ub = [inf inf]; %upper bound
 
 options = optimoptions('linprog','Algorithm','dual-simplex');
-                    
+
 [x1,fval,exitflag,output,lambda] = linprog(c,A,b,[],[],lb,ub,options);
 round(x1)
 profit1 =-c*round(x1)
@@ -47,7 +47,7 @@ profit1 =-c*round(x1)
 %% Part 3
 
 ub = [1000 inf]; %upper bound
-                    
+
 [x2,fval,exitflag,output,lambda] = linprog(c,A,b,[],[],lb,ub,options);
 round(x2)
 profit2 =-c*round(x2)
@@ -57,18 +57,28 @@ profit2 =-c*round(x2)
 bc = (8 + E1) * 10e6; %battery cells
 avs = (22 + E3) * 10e3; %available storage space
 
-c = [(mcr - pr), (mcw - pw), ms];
+c = [(mcr - pr), (mcw - pw)];
 
-A =[br,     bw,     0;
-    rssr,   rssw,   0;
-    hrr,    hrw,    -((5/60)+160)];
+lb = [0 0]; %lower bound
 
-b = [bc, avs, thpm];
+ub = [1000 inf]; %upper bound
+x_3 = 0:72;
 
-lb = [0 0 0]; %lower bound
-
-ub = [1000 inf 72]; %upper bound
-                    
-[x3,fval,exitflag,output,lambda] = linprog(c,A,b,[],[],lb,ub,options);
-round(x3)
-profit3 = -c*round(x3)
+for i = 1:length(x_3)
+    
+    whr = 5/60*x_3(i); % work time reduction per car
+    
+    A =[br,     bw;
+        rssr,   rssw;
+        hrr-whr,    hrw-whr];
+    
+    b = [bc, avs, thpm+160*x_3(i)];
+    
+    [x3(:,i),fval,exitflag,output,lambda] = linprog(c,A,b,[],[],lb,ub,options);
+    round(x3(:,i));
+    profit3(i) = -c*round(x3(:,i))-ms*x_3(i);
+end
+maxIndex = find(profit3 == max(profit3));
+x3(:,maxIndex)
+optWorkers = x_3(maxIndex)
+maxProfit = profit3(maxIndex)
