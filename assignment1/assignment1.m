9aaf4fec817f7250dbb528f96419836b1675e07f
l2late <lucadelaat@gmail.com>
2018-10-02 09:25:06 +0200

    assignment finished?

--- assignment1/assignment1.m
+++ assignment1/assignment1.m
@@ -10,8 +10,10 @@ E3 = 9;
 bc = (5 + E1) * 10e6; %battery cells
 bw = 6*10e3; %battery cells for W
 br = 4*10e3; %battery cells for R
+bv = 2*10e3; %battery cells for V
 hrw = 15; %hours required for W
 hrr = 10; %hours required for R
+hrv = 8;  %hours required for V
 nemp = 100 + E2; %number of employees
 hpm = 160; %hours per month per employee
 thpm = nemp * hpm; % total hours
@@ -19,10 +21,13 @@ ms = 3000 + 50*E3; %monthly salary
 avs = (15 + E3) * 10e3; %available storage space
 rssw = 12; % required storage space W
 rssr = 10; % required storage space R
+rssv = 8;  % required storage space V
 pr = 55000; %price R
 pw = 75000; %price W
+pv = 45000; %price V
 mcr = 30000; %manufacturing cost R
 mcw = 45000; %manufacturing cost W
+mcv = 15000; %manufacturing cost V
 
 
 %% Part 2
@@ -78,7 +83,43 @@ for i = 1:length(x_3)
     round(x3(:,i));
     profit3(i) = -c*round(x3(:,i))-ms*x_3(i);
 end
+
 maxIndex = find(profit3 == max(profit3));
 x3(:,maxIndex)
 optWorkers = x_3(maxIndex)
 maxProfit = profit3(maxIndex)
+
+%% Part 4
+
+c4 = [(mcr - pr), (mcw - pw), (mcv - pv) ];
+
+lb4 = [1250 1000 1500]; %lower bound
+
+ub4 = [inf inf inf]; %upper bound
+
+A4 = [br,        bw,         bv;
+     rssr,      rssw,       rssv;
+     hrr-whr,   hrw-whr,    hrv-whr];
+ 
+b4 = [bc, avs, thpm+160*optWorkers];
+
+[x4,fval,exitflag,output,lambda] = linprog(c4,A4,b4,[],[],lb4,ub4,options);
+
+if isempty(x4)
+    msg = 'It is NOT economically beneficial to build the new model V';
+    warning(msg);
+
+    c5 = [(mcr - pr), (mcw - pw)];
+
+    lb5 = [1250 1000]; %lower bound
+
+    ub5 = [inf inf]; %upper bound
+
+    A5 = [br,       bw;
+         rssr,      rssw;
+         hrr-whr,   hrw-whr];
+
+    b5 = [bc, avs, thpm+160*optWorkers];
+
+    [x5,fval,exitflag,output,lambda] = linprog(c5,A5,b5,[],[],lb5,ub5,options);
+end
