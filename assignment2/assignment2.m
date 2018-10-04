77769dacb4a492bea471cc44d9616c7be6854ed1
nathantim <timmerhout@gmail.com>
2018-10-04 11:34:10 +0200

    Made .m file for assignment 2

---
+++ assignment2/assignment2.m
@@ -0,0 +1,13 @@
+%% Assignment 2 Quadratic Programming
+% Luca de Laat      - 4135040
+% Nathan Timmers    - 4283449
+clear all; close all; clc;
+fprintf('Assignment 2 - Quadratic Programming \n')
+fprintf('Luca de Laat   - 4135040 \n')
+fprintf('Nathan Timmers - 4283449 \n\n')
+%% Initializing Constants
+E1 = 4;
+E2 = 8;
+E3 = 9;
+
+
