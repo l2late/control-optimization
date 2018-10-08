a8d7077b6599b44fc86e13e4cb2819daca696c8f
nathantim <timmerhout@gmail.com>
2018-10-08 15:39:39 +0200

    First attempt at Q1

--- assignment2/assignment2.m
+++ assignment2/assignment2.m
@@ -5,9 +5,34 @@ clear all; close all; clc;
 fprintf('Assignment 2 - Quadratic Programming \n')
 fprintf('Luca de Laat   - 4135040 \n')
 fprintf('Nathan Timmers - 4283449 \n\n')
+
 %% Initializing Constants
 E1 = 4;
 E2 = 8;
 E3 = 9;
 
+%% Question 1
+[~,Qin,Qout,T,Tamb] = importMeasurements('measurements.csv');
+dt = 3600;
 
+dT = Tamb - T;
+dQ = Qin - Qout;
+
+E = Tamb(1:end-1);
+Y = T(2:end);
+
+phi = -[dt*T(1:end-1),dt*dQ(1:end-1)];
+
+H = 2*(phi'*phi);
+c = -2*phi'*Y;
+
+lb = [-0.99 -Inf]';
+ub = [ 0.99  Inf]';
+
+o = optimoptions('quadprog','Algorithm','interior-point-convex');
+[x,~,FLAG] = quadprog(H,c,[],[],[],[],lb,ub,[],o)
+assert(FLAG==1)
+
+alpha1 = x(1);
+alpha2 = x(2);
+T(2:end)-(alpha1*dt*dT(1:end-1) + alpha2*dt*dQ(1:end-1)) - T(1:end-1)
