069b8e36adfec14f82b6cbf31710d36f65dc4439
nathantim <timmerhout@gmail.com>
2018-10-15 14:40:36 +0200

    Work in progress Q3

--- assignment2/assignment2.m
+++ assignment2/assignment2.m
@@ -29,7 +29,7 @@ dQ = Qin - Qout;
 
 E = Tamb(1:endV-1);
 Y = T(2:endV)-T(1:endV-1);
-phi = -[dt*dT(1:endV-1),dt*dQ(1:endV-1)];
+phi = [dt*dT(1:endV-1),dt*dQ(1:endV-1)];
 
 H = 2*(phi'*phi);
 c = -2*phi'*Y;
@@ -38,20 +38,20 @@ lb = [-0.99 -Inf]';
 ub = [ 0.99  Inf]';
 
 o = optimoptions('quadprog','Algorithm','interior-point-convex','Display','off');
-[x,~,FLAG] = quadprog(H,c,[],[],[],[],lb,ub,[],o);
+[x1,~,FLAG] = quadprog(H,c,[],[],[],[],lb,ub,[],o);
 assert(FLAG>0)
 
-a1 = x(1);
-a2 = x(2);
+a1 = x1(1);
+a2 = x1(2);
 A = 1-a1*dt;
-B = [-a2*dt,a2*dt];
+B = [-a2,a2]*dt;
 ck = a1*dt;
 
 max(abs(T(2:endV)-(a1*dt*dT(1:endV-1) + a2*dt*dQ(1:endV-1)) - T(1:endV-1)));
 
 
 fprintf('The values of the system parameters are: \n a1 = %1.4f E-7 \n a2 = %1.4f E-9 \n', a1*1E7, a2*1E9)
-fprintf('Yielding: \n A  =  %1.5f \n B  = %1.5f E-5 \n ck = %1.5f E-4 * Tamb \n\n', A, B*1E5, ck*1E4)
+fprintf('Yielding: \n A  =  %1.5f \n B  = [%1.5f, %1.5f] E-5 \n ck = %1.5f E-4 * Tamb \n\n', A, B(1)*1E5, B(2)*1E5, ck*1E4)
 
 %% Question 3
 fprintf('Question 3: \n')
@@ -62,19 +62,45 @@ inputPricesT = readtable('inputPrices.csv');
 Qout       = heatDemandT{:,2};        % [W]
 inputPrices  = inputPricesT{:,2}; % [EUR/MWh]
 
-N = 360;                % [hours]
-QinMax = (100+E2)*1E3;  % [W]
-T(1) = 330 + E3;        % [K]
-Tamb(:) = 275 + E1;     % [K]
-Tmin = 315;             % [K]
+T = zeros(size(Qout,1),1);
+Tamb = ones(size(Qout,1),1) * (275 + E1);
+
+N = 360;                        % [hours]
+QinMax = (100+E2)*1E3;          % [W]
+T(1) = 330 + E3;                % [K]
+Tmin = 315;                     % [K]
 
 a1 = 1.96E-7;
 a2 = 3.8E-9;
 A = 1-a1*dt;
-B = a2*dt;
+B = [-a2,a2]*dt;
 ck = a1*dt;
-
 
 inputPrices = inputPrices/(1E6*N);  % [EUR/W]
 
-% T(k+1) = A*T(k) + B*
+for k = 1:(size(Qout,1)-1)
+    
+    c = inputPrices(k)*dt;
+    
+    % Equality constraints
+    Aeq = B(2);
+    beq = T(k+1) - A*T(k) - B(1)*Qout(k) - ck*Tamb(k);
+    
+    
+    % Inequality constraints
+    Am = -B(2);
+    b = A*T(k) + B(1)*Qout(k) + ck*Tamb(k);% - Tmin ;
+    
+    lb = 0; %lower bound
+    
+    ub = QinMax; %upper bound
+    
+    options = optimoptions('linprog','Algorithm','dual-simplex','Display','off');
+    
+    [x2,~,exitflag,~,~] = linprog(c,Am,b,Aeq,beq,lb,ub,options);
+    assert(exitflag > 0);
+    
+    T(k+1) = A*T(k) + B*[Qout(k);x2] + ck*Tamb(k);
+
+end
+cost = sum(c'*x2);
