8ba2dd97e65e23c5c2dc1261c413f39030abb391
nathantim <timmerhout@gmail.com>
2018-10-16 11:00:41 +0200

    Small changes

---
+++ assignment3/assignment3.m
@@ -0,0 +1,27 @@
+%% Assignment 2 Quadratic Programming
+% Luca de Laat      - 4135040
+% Nathan Timmers    - 4283449
+clear all; close all; clc;
+fprintf('Assignment 3 - Nonlinear Programming \n')
+fprintf('Luca de Laat   - 4135040 \n')
+fprintf('Nathan Timmers - 4283449 \n\n')
+
+%% Initializing Constants
+E1 = 4;
+E2 = 8;
+E3 = 9;
+
+plotResult = true;
+
+%% Question 1 & 2
+fprintf('Question 1 & 2: \n')
+
+
+% fprintf('The values of the system parameters are: \n a1 = %d \n a2 = %d \n', a1, a2)
+% fprintf('Yielding: \n A  =  %1.5f \n B  = [%d, %d] \n ck = %d * Tamb \n\n', A, B(1), B(2), ck)
+% 
+% clearvars -except E1 E2 E3 dt plotResult
+
+%% Question 3
+fprintf('Question 3: \n')
+
